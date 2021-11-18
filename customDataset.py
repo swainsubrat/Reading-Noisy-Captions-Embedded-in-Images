2de5634ae843f6b2ca923c7b52721aaa65b5e36c
swainsubrat <mailofswainsubrat@gmail.com>
2021-11-18 22:43:49 +0530

    added train, model, utils

--- customDataset.py
+++ customDataset.py
@@ -7,33 +7,48 @@ import pickle
 from torchvision import transforms as T
 from PIL import Image
 
+from utils import load
+
 class ImageAndCaptionsDataset(Dataset):
     # caption_path is the processed caption path 
-    def __init__(self, image_path="data/train_data",caption_path="processed_captions.tsv", transform=None):
+    def __init__(self, image_path="data/",caption_path="./objects/processed_captions.pkl", transform=None):
         super(ImageAndCaptionsDataset, self).__init__()
-        self.image_filenames
+        self.transform = transform
         self.image_path=image_path
-        self.caption_path=caption_path
-        dict=pickle.load(self.caption_path)
+        _dict=load(caption_path)
 
-        self.image_filenames=dict["image_filenames"]
-        self.caption_lengths=dict["caption_lengths"]    
-        self.max_length=dict["max_caption_length"]
-        self.captions=dict["captions"]
-        self.word_map=dict["word_map"]
+        self.image_filenames=_dict["image_filenames"]
+        self.caption_lengths=_dict["caption_lengths"]    
+        self.max_length=_dict["max_caption_length"]
+        self.captions=_dict["captions"]
+        self.word_map=_dict["word_map"]
 
         
     def __getitem__(self, idx: int):
+
         image_path = os.path.join(self.image_path,self.image_filenames[idx])
         image = Image.open(image_path).convert("RGB")
-        image = np.array(image) 
+        image = np.array(image)
         image=T.ToTensor()(image)
-        if self.transform is not None:
-            image = self.transform(image)
-        caption = torch.LongTensor(self.captions[idx])
+
+        # "TODO": Sweta: what is this self.transform function
+        # if self.transform is not None:
+        #     image = self.transform(image)
+
+        caption = torch.LongTensor([self.captions[idx]])
         caption_length = torch.LongTensor([self.caption_lengths[idx]])
-        return image,caption, caption_length
+        return image, caption, caption_length
  
     def __len__(self):
         return len(self.captions)
 
+
+if __name__ == "__main__":
+    ic_dataset = ImageAndCaptionsDataset()
+    data_loader_test = torch.utils.data.DataLoader(
+        ic_dataset, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=None)
+
+    for i, (imgs, caps, caplens) in enumerate(data_loader_test):
+        print(imgs, caps, caplens)
+        break
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import pickle
 import numpy as np
 import pandas as pd
@@ -6,7 +7,7 @@ import pandas as pd
 from constants import *
 from pprint import pprint
 
-from typing import List, Tuple
+from typing import Any, List, Tuple
 from collections import Counter
 
 
@@ -34,7 +35,6 @@ def get_word_map(captions: pd.Series) -> Tuple:
 
     return max_caption_length,caption_lengths, word_map
 
-
 def pad_and_append(captions: pd.Series, max_caption_length: int) -> pd.Series:
     """
     To make all the token length equal with padding
@@ -52,16 +52,125 @@ def pad_and_append(captions: pd.Series, max_caption_length: int) -> pd.Series:
     
     return padded_captions
 
+def encoded_captions(padded_captions,caption_lengths,word_map):
+    enc_captions = []
+    for i,caption in enumerate(padded_captions):
+        enc_caption = [word_map['<start>']] + [word_map.get(word, word_map['<unk>']) for word in caption] + [
+                        word_map['<end>']] + [word_map['<pad>']] * (max_len - caption_lengths[i])
+        enc_captions.append(enc_caption)
+    return enc_captions
+
+def save(obj: Any, path: str) -> None:
+    outfile = open(path, 'wb')
+    pickle.dump(obj, outfile)
+    outfile.close()
+
+def load(path: str) -> None:
+    infile = open(path, 'rb')
+    obj = pickle.load(infile)
+    infile.close()
+
+    return obj
+
+def clip_gradient(optimizer, grad_clip):
+    """
+    Clips gradients computed during backpropagation to avoid explosion of gradients.
+    :param optimizer: optimizer with the gradients to be clipped
+    :param grad_clip: clip value
+    """
+    for group in optimizer.param_groups:
+        for param in group['params']:
+            if param.grad is not None:
+                param.grad.data.clamp_(-grad_clip, grad_clip)
+
+def save_checkpoint(data_name, epoch, epochs_since_improvement, encoder, decoder, encoder_optimizer, decoder_optimizer,
+                    bleu4, is_best):
+    """
+    Saves model checkpoint.
+    :param data_name: base name of processed dataset
+    :param epoch: epoch number
+    :param epochs_since_improvement: number of epochs since last improvement in BLEU-4 score
+    :param encoder: encoder model
+    :param decoder: decoder model
+    :param encoder_optimizer: optimizer to update encoder's weights, if fine-tuning
+    :param decoder_optimizer: optimizer to update decoder's weights
+    :param bleu4: validation BLEU-4 score for this epoch
+    :param is_best: is this checkpoint the best so far?
+    """
+    state = {'epoch': epoch,
+             'epochs_since_improvement': epochs_since_improvement,
+             'bleu-4': bleu4,
+             'encoder': encoder,
+             'decoder': decoder,
+             'encoder_optimizer': encoder_optimizer,
+             'decoder_optimizer': decoder_optimizer}
+    filename = 'checkpoint_' + data_name + '.pth.tar'
+    torch.save(state, filename)
+    # If this checkpoint is the best so far, store a copy so it doesn't get overwritten by a worse checkpoint
+    if is_best:
+        torch.save(state, 'BEST_' + filename)
+class AverageMeter(object):
+    """
+    Keeps track of most recent, average, sum, and count of a metric.
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def adjust_learning_rate(optimizer, shrink_factor):
+    """
+    Shrinks learning rate by a specified factor.
+    :param optimizer: optimizer whose learning rate must be shrunk.
+    :param shrink_factor: factor in interval (0, 1) to multiply learning rate with.
+    """
+
+    print("\nDECAYING learning rate.")
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = param_group['lr'] * shrink_factor
+    print("The new learning rate is %f\n" % (optimizer.param_groups[0]['lr'],))
+
+def accuracy(scores, targets, k):
+    """
+    Computes top-k accuracy, from predicted and true labels.
+    :param scores: scores from the model
+    :param targets: true labels
+    :param k: k in top-k accuracy
+    :return: top-k accuracy
+    """
+
+    batch_size = targets.size(0)
+    _, ind = scores.topk(k, 1, True, True)
+    correct = ind.eq(targets.view(-1, 1).expand_as(ind))
+    correct_total = correct.view(-1).float().sum()  # 0D tensor
+    return correct_total.item() * (100.0 / batch_size)
+
 if __name__ == "__main__":
     df: pd.DataFrame    = pd.read_csv(caption_path ,names=["filenames", "captions"], sep='\t', header=None).head(50)
-    captions: pd.Series = df["captions"]
-    image_filenames=df["filenames"].to_list()
-    max_caption_length,caption_lengths, word_map = get_word_map(captions)
-    captions = pad_and_append(captions, max_caption_length)
-    #dump the dict using pickle
-    #int,list,dict,list,list
-    dict={"max_caption_length":max_caption_length,"caption_lengths":caption_lengths,
-    "word_map":word_map,"captions":captions,"image_filenames":image_filenames}
+    captions: pd.Series = df["captions"][:10]
+    image_filenames     = df["filenames"].to_list()
 
-    print([len(c.split()) for c in captions])
-    print(captions)
+    max_caption_length, caption_lengths, word_map = get_word_map(captions)
+    padded_captions = pad_and_append(captions, max_caption_length)
+    encoded_caption = encoded_captions(padded_captions, caption_lengths, word_map)
+
+    _dict = {
+        "max_caption_length": max_caption_length,
+        "caption_lengths": caption_lengths,
+        "word_map": word_map,
+        "captions": encoded_caption,
+        "image_filenames": image_filenames
+    }
+
+    save(_dict, "./objects/processed_captions.pkl")
